9fbf6464b0ea70b4bbd576cb18e3c72b28214f0b
Muzammil Sheikh <muzammil26122004@gmail.com>
2024-09-21 21:42:18 +0500

    BMI calc

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightInKg = 52;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters ** 2);
+console.log(`your BMI is ${bmi}`);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKg = 52;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters**2);
+console.log(`your BMI is ${bmi}`);
